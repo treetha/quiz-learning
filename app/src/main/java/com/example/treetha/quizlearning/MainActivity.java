5046a25d89ced39441c54c859174a70a23d3d2a8
Treetha.t <treetha.tt@gmail.com>
2016-12-26 17:47:08 +0700

    add second page

--- app/src/main/java/com/example/treetha/quizlearning/MainActivity.java
+++ app/src/main/java/com/example/treetha/quizlearning/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.treetha.quizlearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button choiceTestButton, dragTestButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        choiceTestButton = (Button) findViewById(R.id.test_choice_button);
+        dragTestButton = (Button) findViewById(R.id.test_drag_button);
+
+        choiceTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestChoiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dragTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/treetha/quizlearning/TestChoiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.treetha.quizlearning;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by treetha on 12/26/16.
+ */
+public class TestChoiceActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_choice_activity);
+    }
+}
